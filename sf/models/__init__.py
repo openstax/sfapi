e4e5106ea788f7a020404e64c0d4edd2cf54040b
Michael Harrison <volo@rice.edu>
2024-02-15 12:35:52 -0600

    sf models and abstract user class

---
+++ sf/models/__init__.py
@@ -0,0 +1,3 @@
+# These models map to our Salesforce Objects
+# Each query to these models generates an API call - be careful!
+# Only add fields here that are needed for the purpose of the API to keep sanity while maintaining
