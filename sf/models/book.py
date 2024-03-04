27e5936621ac21e43208fab51f0d115eb9f9bcfd
Michael Harrison <volo@rice.edu>
2024-03-04 10:54:00 -0600

    fix sf book name

--- sf/models/book.py
+++ sf/models/book.py
@@ -5,7 +5,7 @@ class Book(models.SalesforceModel):
     name = models.CharField(max_length=255)
     official_name = models.CharField(max_length=255, db_column='Official_Name__c')
     type = models.CharField(max_length=255, db_column='Book_Type__c')
-    subject_areas = models.CharField(max_length=255, db_column='OSWeb_Group__c')
+    subject_areas = models.CharField(max_length=255, db_column='Subject_Area__c')
     website_url = models.URLField(max_length=255, db_column='OSC_URL__c')
     active_book = models.BooleanField(default=True, db_column='Active__c')
 
