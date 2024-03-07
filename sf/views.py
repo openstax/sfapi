8b4f825df98311d63bbb5e5a0a2ee0d40ca3d581
Michael Harrison <volo@rice.edu>
2024-03-06 20:43:50 -0600

    add release info

---
+++ sf/views.py
@@ -0,0 +1,31 @@
+from sentry_sdk import capture_message
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.db import connections
+from django.conf import settings
+from salesforce.dbapi.driver import ApiUsage
+
+
+def info(request):
+    return JsonResponse({
+        'release_information': release_information(),
+
+    })
+
+def sf_api_usage():
+    api_usage = connections['salesforce'].connection.api_usage
+
+    if api_usage.api_usage / api_usage.api_limit > 0.5:
+        capture_message(f"Salesforce API usage is at {api_usage.api_usage / api_usage.api_limit * 100}%")
+
+    return {
+        'api_usage': api_usage.api_usage,
+        'api_limit': api_usage.api_limit,
+    }
+
+def release_information():
+    return {
+        'sfapi_version': settings.RELEASE_VERSION,
+        'deployment_version': settings.DEPLOYMENT_VERSION,
+        'environment': settings.ENVIRONMENT,
+    }
--- sfapi/urls.py
+++ sfapi/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from api.api_v1 import api
+from sf.views import info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('info/', info),
     path('api/v1/', api.urls),
 ]
