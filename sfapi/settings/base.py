48335ab8d139c37293b88e4076e9b064e4b3272e
Michael Volo <volo@rice.edu>
2024-06-08 11:10:52 -0500

    Update base.py

--- sfapi/settings/base.py
+++ sfapi/settings/base.py
@@ -140,7 +140,7 @@ DATABASES = {
 
 # Set Salesforce environment based on the host for /info/
 # username, accounting for email period, then env. if the connecting user has a period in their email, this will fail
-SALESFORCE_ENVIRONMENT = DATABASES['salesforce']['USER'].split('.')[2]
+SALESFORCE_ENVIRONMENT = os.getenv('SALESFORCE_USERNAME', 'user@rice.edu.staging').split('.')[2]
 
 # Salesforce API rate limiting
 SALESFORCE_API_RATE_LIMIT = os.getenv('SALESFORCE_API_RATE_LIMIT', '20/min')  # x/sec x/min x/hour
