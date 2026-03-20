ff4a97d9ec5ba9703b5c7349539daa0f3db80ce9
Michael Volo <volo@rice.edu>
2026-03-20 10:39:42 -0500

    Update urls.py

--- sfapi/urls.py
+++ sfapi/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 from api.api_v1 import api
 
 urlpatterns = [
+    path("ping", lambda request: HttpResponse()),
     path("admin/", admin.site.urls),
     path("api/v1/", api.urls),
     path("pardot/", include("pardot.urls")),
